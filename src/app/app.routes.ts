84c35cd85f05f2ee89bd3a40b9ffd9963489b845
Daria Deriagina <w0862447@myscc.ca>
2024-12-05 15:03:26 -0500

    Assignment 11 DONE

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
-import { Routes } from '@angular/router';
+import { Route } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { DetailsComponent } from './details/details.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Route[] = [
+  { path: '', component: HomeComponent },
+  { path: 'details/:id', component: DetailsComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+];
